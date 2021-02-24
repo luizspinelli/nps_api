8c98b4a92f06ad446448f1a4ae36df04ce41c897
Luiz Spinelli <luiz.lucchi@gmail.com>
2021-02-24 20:35:02 -0300

    users controller, router and model

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,13 @@ import express from 'express'
 
 import './database'
 
+import router from './routes'
+
 const app = express()
 
-app.get('/', (request, response)=>{
-  return response.json({"message": "Hello World"})
-})
+app.use(express.json())
 
-app.post('/', (request, response)=>{
-  return response.json({"message": "Dados salvos com sucesso"})
-})
+app.use(router)
 
 app.listen(3333, ()=>{
   console.log('Server started on port 3333')
