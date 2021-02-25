f2d0727df164da5bb4c5fcb4171888dc170bc673
Luiz Spinelli <luiz.lucchi@gmail.com>
2021-02-25 20:29:15 -0300

    refatorando users controller

---
+++ src/repositories/UsersRepository.ts
@@ -0,0 +1,8 @@
+import { EntityRepository, Repository } from "typeorm";
+import User from "../models/User";
+
+
+@EntityRepository(User)
+export default class UsersRepository extends Repository<User> {
+
+}
